9a86950dbbf98c9c697c5330ece1a39f7b47e5d8
Emre Can Duygulu <ecduygulu@gmail.com>
2025-06-30 00:05:02 +0200

    region to account id

--- agents/generate.py
+++ agents/generate.py
@@ -18,7 +18,7 @@ import requests
 def get_s3_client():
     return boto3.client(
         "s3",
-        endpoint_url=f"https://{os.environ['R2_REGION']}.r2.cloudflarestorage.com",
+        endpoint_url=f"https://{os.environ['R2_ACCOUNT_ID']}.r2.cloudflarestorage.com",
         aws_access_key_id=os.environ["R2_ACCESS_KEY_ID"],
         aws_secret_access_key=os.environ["R2_SECRET_ACCESS_KEY"],
     )
